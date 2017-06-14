61c8622d53781baa2183573dd92b93bc0c791408
Spiel's Macmini <macmini@spielss-mac-mini.local>
2017-06-14 16:51:36 +0530

    14june

---
+++ Haraj_app/AllTablescroll/YSLScrollMenuView1.m
@@ -0,0 +1,197 @@
+//
+//  YSLScrollMenuView.m
+//  YSLContainerViewController
+//
+//  Created by yamaguchi on 2015/03/03.
+//  Copyright (c) 2015年 h.yamaguchi. All rights reserved.
+//
+
+#import "YSLScrollMenuView1.h"
+
+static const CGFloat kYSLScrollMenuView1Width  = 90;
+static const CGFloat kYSLScrollMenuView1Margin = 10;
+static const CGFloat kYSLIndicatorHeight = 3;
+
+@interface YSLScrollMenuView1 ()
+
+
+@property (nonatomic, strong) UIView *indicatorView;
+
+@end
+
+@implementation YSLScrollMenuView1
+
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // default
+        _viewbackgroudColor = [UIColor whiteColor];
+        _itemfont = [UIFont systemFontOfSize:16];
+        _itemTitleColor = [UIColor colorWithRed:0.866667 green:0.866667 blue:0.866667 alpha:1.0];
+        _itemSelectedTitleColor = [UIColor colorWithRed:0.333333 green:0.333333 blue:0.333333 alpha:1.0];
+        // _itemIndicatorColor = [UIColor colorWithRed:0.168627 green:0.498039 blue:0.839216 alpha:1.0];
+        
+        self.backgroundColor = _viewbackgroudColor;
+        _scrollView = [[UIScrollView alloc]initWithFrame:self.bounds];
+        _scrollView.showsHorizontalScrollIndicator = NO;
+        [self addSubview:_scrollView];
+    }
+    return self;
+}
+
+#pragma mark -- Setter
+
+- (void)setViewbackgroudColor:(UIColor *)viewbackgroudColor
+{
+    if (!viewbackgroudColor) { return; }
+    _viewbackgroudColor = viewbackgroudColor;
+    self.backgroundColor = viewbackgroudColor;
+}
+
+- (void)setItemfont:(UIFont *)itemfont
+{
+    if (!itemfont) { return; }
+    _itemfont = itemfont;
+    for (UILabel *label in _itemTitleArray) {
+        label.font = itemfont;
+    }
+}
+
+- (void)setItemTitleColor:(UIColor *)itemTitleColor
+{
+    if (!itemTitleColor) { return; }
+    _itemTitleColor = itemTitleColor;
+    for (UILabel *label in _itemTitleArray) {
+        label.textColor = itemTitleColor;
+    }
+}
+
+- (void)setItemIndicatorColor:(UIColor *)itemIndicatorColor
+{
+    if (!itemIndicatorColor) { return; }
+    _itemIndicatorColor = itemIndicatorColor;
+    _indicatorView.backgroundColor = itemIndicatorColor;
+}
+
+- (void)setItemTitleArray:(NSArray *)itemTitleArray
+{
+    if (_itemTitleArray != itemTitleArray) {
+        _itemTitleArray = itemTitleArray;
+        NSMutableArray *views = [NSMutableArray array];
+        
+        for (int i = 0; i < itemTitleArray.count; i++) {
+            CGRect frame = CGRectMake(0, 0, kYSLScrollMenuView1Width, CGRectGetHeight(self.frame));
+            UILabel *itemView = [[UILabel alloc] initWithFrame:frame];
+            [self.scrollView addSubview:itemView];
+            itemView.tag = i;
+            itemView.text = itemTitleArray[i];
+            itemView.userInteractionEnabled = YES;
+            itemView.backgroundColor = [UIColor clearColor];
+            itemView.textAlignment = NSTextAlignmentCenter;
+            itemView.font = self.itemfont;
+            itemView.textColor = _itemTitleColor;
+            [views addObject:itemView];
+            
+            UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(itemViewTapAction:)];
+            [itemView addGestureRecognizer:tapGesture];
+        }
+        
+        self.itemViewArray = [NSArray arrayWithArray:views];
+        
+        // indicator
+        _indicatorView = [[UIView alloc]init];
+        _indicatorView.frame = CGRectMake(10, _scrollView.frame.size.height - kYSLIndicatorHeight, kYSLScrollMenuView1Width, kYSLIndicatorHeight);
+        _indicatorView.backgroundColor = self.itemIndicatorColor;
+        [_scrollView addSubview:_indicatorView];
+    }
+}
+
+#pragma mark -- public
+
+- (void)setIndicatorViewFrameWithRatio:(CGFloat)ratio isNextItem:(BOOL)isNextItem toIndex:(NSInteger)toIndex
+{
+    CGFloat indicatorX = 0.0;
+    if (isNextItem) {
+        indicatorX = ((kYSLScrollMenuView1Margin + kYSLScrollMenuView1Width) * ratio ) + (toIndex * kYSLScrollMenuView1Width) + ((toIndex + 1) * kYSLScrollMenuView1Margin);
+    } else {
+        indicatorX =  ((kYSLScrollMenuView1Margin + kYSLScrollMenuView1Width) * (1 - ratio) ) + (toIndex * kYSLScrollMenuView1Width) + ((toIndex + 1) * kYSLScrollMenuView1Margin);
+    }
+    
+    if (indicatorX < kYSLScrollMenuView1Margin || indicatorX > self.scrollView.contentSize.width - (kYSLScrollMenuView1Margin + kYSLScrollMenuView1Width)) {
+        return;
+    }
+    _indicatorView.frame = CGRectMake(indicatorX, _scrollView.frame.size.height - kYSLIndicatorHeight, kYSLScrollMenuView1Width, kYSLIndicatorHeight);
+    //  NSLog(@"retio : %f",_indicatorView.frame.origin.x);
+}
+
+- (void)setItemTextColor:(UIColor *)itemTextColor
+    seletedItemTextColor:(UIColor *)selectedItemTextColor
+            currentIndex:(NSInteger)currentIndex
+{
+    if (itemTextColor) { _itemTitleColor = itemTextColor; }
+    if (selectedItemTextColor) { _itemSelectedTitleColor = selectedItemTextColor; }
+    
+    for (int i = 0; i < self.itemViewArray.count; i++) {
+        UILabel *label = self.itemViewArray[i];
+        if (i == currentIndex) {
+            label.alpha = 0.0;
+            [UIView animateWithDuration:0.75
+                                  delay:0.0
+                                options:UIViewAnimationOptionCurveLinear | UIViewAnimationOptionAllowUserInteraction
+                             animations:^{
+                                 label.alpha = 1.0;
+                                 label.textColor = _itemSelectedTitleColor;
+                             } completion:^(BOOL finished) {
+                             }];
+        } else {
+            label.textColor = _itemTitleColor;
+        }
+    }
+}
+
+#pragma mark -- private
+
+// menu shadow
+- (void)setShadowView
+{
+    //    UIView *view = [[UIView alloc]init];
+    //    view.frame = CGRectMake(0, self.frame.size.height - 0.5, CGRectGetWidth(self.frame), 0.5);
+    //    view.backgroundColor = [UIColor lightGrayColor];
+    //    [self addSubview:view];
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    CGFloat x = kYSLScrollMenuView1Margin;
+    for (NSUInteger i = 0; i < self.itemViewArray.count; i++) {
+        CGFloat width = kYSLScrollMenuView1Width;
+        UIView *itemView = self.itemViewArray[i];
+        itemView.frame = CGRectMake(x, 0, width, self.scrollView.frame.size.height);
+        x += width + kYSLScrollMenuView1Margin;
+    }
+    self.scrollView.contentSize = CGSizeMake(x, self.scrollView.frame.size.height);
+    
+    CGRect frame = self.scrollView.frame;
+    if (self.frame.size.width > x) {
+        frame.origin.x = (self.frame.size.width - x) / 2;
+        frame.size.width = x;
+    } else {
+        frame.origin.x = 0;
+        frame.size.width = self.frame.size.width;
+    }
+    self.scrollView.frame = frame;
+}
+
+#pragma mark -- Selector --------------------------------------- //
+- (void)itemViewTapAction:(UITapGestureRecognizer *)Recongnizer
+{
+    if (self.delegate && [self.delegate respondsToSelector:@selector(scrollMenuViewSelectedIndex:)]) {
+        [self.delegate scrollMenuViewSelectedIndex:[(UIGestureRecognizer*) Recongnizer view].tag];
+    }
+}
+
+@end
